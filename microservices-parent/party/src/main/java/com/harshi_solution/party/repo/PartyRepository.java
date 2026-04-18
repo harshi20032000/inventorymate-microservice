3cfaa5355dc1ad17d9321d3f3c39492ec7c0cc2e
Harshikesh <harshibabu604@gmail.com>
2026-04-18 18:09:14 +0530

    auth module added into Product.

--- microservices-parent/party/src/main/java/com/harshi_solution/party/repo/PartyRepository.java
+++ microservices-parent/party/src/main/java/com/harshi_solution/party/repo/PartyRepository.java
@@ -3,9 +3,11 @@ package com.harshi_solution.party.repo;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.harshi_solution.party.entities.Party;
 
+@Repository
 public interface PartyRepository extends JpaRepository<Party, Long> {
 
     Optional<Party> findByPartyName(String partyName);
--- microservices-parent/product/src/main/java/com/harshi_solution/product/ProductApplication.java
+++ microservices-parent/product/src/main/java/com/harshi_solution/product/ProductApplication.java
@@ -3,7 +3,7 @@ package com.harshi_solution.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.harshi_solution")
 public class ProductApplication {
 
 	public static void main(String[] args) {
