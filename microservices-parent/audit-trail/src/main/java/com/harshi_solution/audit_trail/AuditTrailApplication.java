cb4b9443e7b6cfd09fd060e557cf5153401e2a57
Harshikesh <harshibabu604@gmail.com>
2026-04-25 05:14:06 +0530

    audit trail service added.

---
+++ microservices-parent/audit-trail/src/main/java/com/harshi_solution/audit_trail/AuditTrailApplication.java
@@ -0,0 +1,13 @@
+package com.harshi_solution.audit_trail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditTrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditTrailApplication.class, args);
+	}
+
+}
---
+++ microservices-parent/audit-trail/src/test/java/com/harshi_solution/audit_trail/AuditTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshi_solution.audit_trail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
