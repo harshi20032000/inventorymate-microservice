e3f8282c02f64c4809934688d0dbceba3ddb45ea
Harshikesh <harshibabu604@gmail.com>
2026-04-26 02:07:37 +0530

    enableFeignClient added with basePackages=com.harshi_solution.auth.audit

--- microservices-parent/party/src/main/java/com/harshi_solution/party/PartyApplication.java
+++ microservices-parent/party/src/main/java/com/harshi_solution/party/PartyApplication.java
@@ -2,8 +2,10 @@ package com.harshi_solution.party;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.harshi_solution")
+@EnableFeignClients(basePackages = "com.harshi_solution.auth.audit")
 public class PartyApplication {
 
 	public static void main(String[] args) {
--- microservices-parent/warehouse/src/main/java/com/harshi_solution/warehouse/WarehouseApplication.java
+++ microservices-parent/warehouse/src/main/java/com/harshi_solution/warehouse/WarehouseApplication.java
@@ -2,8 +2,10 @@ package com.harshi_solution.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.harshi_solution")
+@EnableFeignClients(basePackages = "com.harshi_solution.auth.audit")
 public class WarehouseApplication {
 
 	public static void main(String[] args) {
